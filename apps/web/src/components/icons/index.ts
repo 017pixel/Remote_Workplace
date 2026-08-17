f3485317bf51d47a558f666988a56fd316bbfe16
Benjamin Becker <beckerbenjamin2010@gmail.com>
2026-08-17 16:46:00 +0200

    feat: route product icons through monochrome palette

--- apps/web/src/components/icons/index.ts
+++ apps/web/src/components/icons/index.ts
@@ -1,1 +1,27 @@
 export * from "./WorkbenchIcons";
+
+// Explicit product exports override the legacy colorful glyphs while keeping
+// all utility/action icons source-compatible. New product/navigation icons
+// should be added to ProductIcons instead of introducing vendor-colored SVGs.
+export {
+  RemoteWorkbenchIcon,
+  DashboardIcon,
+  WorkbenchIcon,
+  TechTldrsIcon,
+  ProjekteIcon,
+  TerminalIcon,
+  PreviewsIcon,
+  GalerieIcon,
+  FinderIcon,
+  SkillsIcon,
+  BrowserIcon,
+  NutzungIcon,
+  EinstellungenIcon,
+  T3CodeIcon,
+  HermesIcon,
+  CodeServerIcon,
+  OpenCodeIcon,
+  CodexIcon,
+  ClaudeCodeIcon,
+  ExtensionsIcon,
+} from "./ProductIcons";
